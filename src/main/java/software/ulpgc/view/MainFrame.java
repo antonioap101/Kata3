aafcb0941312e0ff2bfcfe25549106b1618ab895
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-01-03 19:18:38 +0000

    Implemented MainFrame Class

--- src/main/java/software/ulpgc/view/MainFrame.java
+++ src/main/java/software/ulpgc/view/MainFrame.java
@@ -4,15 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private JFreeChartHistogramDisplay histogramDisplay;
+
     public MainFrame() throws HeadlessException {
         setTitle("Histogram");
         setSize(1000,800);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        add(createHistogramDisplay())
+        add(createHistogramDisplay());
     }
 
     private Component createHistogramDisplay() {
+        histogramDisplay = new JFreeChartHistogramDisplay();
+        return histogramDisplay;
+    }
 
+    public JFreeChartHistogramDisplay getHistogramDisplay() {
+        return histogramDisplay;
     }
 }
